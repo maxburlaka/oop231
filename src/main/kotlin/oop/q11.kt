e024bcd60d0f561f49b874655fc47d5ba8139add
Max Burlaka <maksbyrlaka69@gmail.com>
2021-10-14 18:46:39 +0300

    secondCommit

--- src/main/kotlin/oop/q11.kt
+++ src/main/kotlin/oop/q11.kt
@@ -1,20 +1,40 @@
 package oop
 
 abstract class Shape(){
-    var color = "Green"
 
-    fun draw(){
-
+    open fun draw(){
+        var color = "Green"
+        println("Фігура намальована колір $color")
     }
 
 }
 class Circle(): Shape(){
 
-
+    val a = 112
+    val b = 170
+    val c = 110
+    override fun draw() {
+        var color = "Green"
+        println("Фігура намальована колір $color")
+    }
 }
 class Rectangle(): Shape(){
 
+    val a = 110
+    val b = 130
+    val c = 142
+
+    override fun draw() {
+        var color = "Green"
+        println("Фігура намальована колір $color")
+    }
 }
 
 fun main(){
+
+    val ci = Circle()
+    ci.draw()
+
+    val re = Rectangle()
+    re.draw()
 }
